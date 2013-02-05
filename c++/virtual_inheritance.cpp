ed4150a44ad08ce5853e5ac95c36a82aa42d70e7
Pascal Spörri <pascal@spoerri.io>
2013-02-05 15:43:45 +0100

    Function override

--- c++/virtual_inheritance.cpp
+++ c++/virtual_inheritance.cpp
@@ -23,21 +23,25 @@ public:
 };
 
 class Programmer : virtual public Person {
+    bool windows_user;
     public:
-        Programmer():
+        Programmer(bool z):
+            windows_user(z),
             Person(false)
             {
             }
         virtual void type() {
             cout << "Programmer ";
+            if (windows_user) {
+                cout << "is a windows user and ";
+            }
         }
 };
 
 class PseudoProgrammer : virtual public Programmer {
-
 public:
     PseudoProgrammer():
-        Programmer(),
+        Programmer(true),
         Person(true)
     {
         //likesDiamonds = true;
@@ -47,18 +51,43 @@ public:
     }
 };
 
+class PHPProgrammer : virtual public Programmer {
+public:
+    PHPProgrammer():
+        Person(true),
+        Programmer(true)
+    {
+        //likesDiamonds = true;
+    }
+    void type() {
+        cout << "PHP Programmer ";
+    }
+};
+
 int main() {
     Person p(true);
     p.status();
 
-    Programmer pr;
+    Programmer pr(false);
     pr.status();
 
     PseudoProgrammer psp;
     psp.status();
 
+    PHPProgrammer php;
+    php.status();
+
     pr = psp;
-    pr.status();
+    pr.status(); // object slicing
+
+    Programmer* prpointer = new Programmer(false);
+    prpointer = &psp;
+    prpointer->status();
 }
 
-
+//  Person likes diamonds
+//  Programmer dislikes diamonds
+//  Pseudo Programmer likes diamonds
+//  PHP Programmer likes diamonds
+//  Programmer is a windows user likes diamonds
+//  Pseudo Programmer likes diamonds
