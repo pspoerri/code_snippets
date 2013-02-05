fdd70711df6e8d11f6f0f3947ff05afd75e43b90
Pascal Spörri <pascal@spoerri.io>
2013-02-05 15:26:13 +0100

    Virtual Inheritance

---
+++ c++/virtual_inheritance.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+class Person {
+    bool likesDiamonds;
+
+public: 
+    virtual void type() {
+        cout << "Person ";
+    }
+    void status() {
+        type();
+        if (likesDiamonds) {
+            cout << "likes";
+        } else {
+            cout << "dislikes";
+        }
+        cout << " diamonds" << endl;
+    }
+    Person(bool l) {
+        likesDiamonds = l;
+    }
+};
+
+class Programmer : virtual public Person {
+    public:
+        Programmer():
+            Person(false)
+            {
+            }
+        void type() {
+            cout << "Programmer ";
+        }
+};
+
+class PseudoProgrammer : virtual public Programmer {
+
+public:
+    PseudoProgrammer():
+        Programmer(),
+        Person(true)
+    {
+        //likesDiamonds = true;
+    }
+    void type() {
+        cout << "Pseudo Programmer ";
+    }
+};
+
+int main() {
+    Person p(true);
+    p.status();
+
+    Programmer pr;
+    pr.status();
+
+    PseudoProgrammer psp;
+    psp.status();
+}
+
+
